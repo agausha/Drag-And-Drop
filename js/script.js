c48913062956bbcc630a4a09ca7ba6c18ae0825e
Isaac <uniqucode@gmail.com>
2022-08-20 11:24:11 +0100

    Refrencing listColumns

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
 const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
+// Item Lists
+const listColumns = document.querySelectorAll('.drag-item-list');
