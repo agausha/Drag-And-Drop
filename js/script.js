99af73d44c0cf200767a0c0800afdb21de35d0f7
Isaac <uniqucode@gmail.com>
2022-08-20 13:32:43 +0100

    Looping through arrayNames

--- js/script.js
+++ js/script.js
@@ -35,4 +35,7 @@ function getSavedColumns() {
 function updateSavedColumns() {
   listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
   const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
+  });
 }
