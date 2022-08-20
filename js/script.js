2ffac51d1e904bf2aa986ad4bdf3f5938cc15371
Isaac <uniqucode@gmail.com>
2022-08-20 11:43:41 +0100

    Initialize backlogListArray

--- js/script.js
+++ js/script.js
@@ -8,3 +8,6 @@ const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
+
+// Initialize Arrays
+let backlogListArray = [];
