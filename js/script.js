39c84ba8f544ec60354a848d22fc67f2ca765450
Isaac <uniqucode@gmail.com>
2022-08-20 11:21:34 +0100

    Refrencing addItemContainers

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
 const saveItemBtns = document.querySelectorAll('.solid');
+const addItemContainers = document.querySelectorAll('.add-container');
